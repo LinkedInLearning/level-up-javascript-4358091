452743fd35f815556be271be06722a000103c4a0
Angie Spears <angie.spears809@gmail.com>
2023-12-07 18:07:06 +0000

    solution

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,20 @@
 // Write your code here
+// getStudents takes in one arg classroom
+// classroom  contains a boolean value hasTeachingAssistant, classlist 
+
+const getStudents = (classroom) => {
+  const {hasTeachingAssistant, classList} = classroom
+  if (hasTeachingAssistant) {
+    return classList.slice(2)
+  } 
+
+  return classList.slice(1)
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));
+
+
+
