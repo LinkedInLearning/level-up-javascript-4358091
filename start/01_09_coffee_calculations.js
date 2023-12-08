7817a759644d0b1d8d19c014dd1467eebd0bdc4c
Angie Spears <angie.spears809@gmail.com>
2023-12-08 00:58:56 +0000

    add solutions

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,14 @@
 // Write your code here
+
+
+
+function coffeeSum(coffeeOrders) {
+  const coffeePrice = 1.25
+  const coffeeTotal = coffeeOrders.reduce((acc, order, index) => {
+  acc += (coffeePrice * order)
+  return acc
+  }, 0)
+  return `The total bill is $${coffeeTotal}`
+}
+const coffeeOrders = [2, 5, 7, 1, 4]
+console.log('coffeeSum', coffeeSum(coffeeOrders))
